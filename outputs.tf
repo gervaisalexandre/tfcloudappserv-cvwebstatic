adf64eabcecd03e95b6b923c4bf801b04b46540d
gervais alexandre <tsalambalapierre@outlook.fr>
2025-07-24 02:15:07 +0100

    recorrection outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
 output "app_url" {
-  value = "https://${azurerm_linux_web_app.app.host_names[0]}"
-  description = "URL de l’application Web Linux déployée"
+  value = "https://${var.app_name}.azurewebsites.net"
 }
